5d0506c5811478f95eeb1fe19d81951a6aeebfb6
Jerry Xie <jerryxie@microsoft.com>
2025-09-11 11:07:40 -0500

    rt633 example: add flexspi-nor-flash example

---
+++ examples/rt633/src/bin/flexspi-nor-flash.rs
@@ -0,0 +1,81 @@
+#![no_std]
+#![no_main]
+
+use defmt::{error, info};
+use embassy_executor::Spawner;
+use embassy_imxrt::flexspi::nor_flash::FlexSpiNorFlash;
+use embassy_imxrt::gpio;
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _, rt633_examples as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let mut p = embassy_imxrt::init(Default::default());
+
+    info!("Hello world");
+
+    let mut led = gpio::Output::new(
+        p.PIO1_5,
+        gpio::Level::Low,
+        gpio::DriveMode::PushPull,
+        gpio::DriveStrength::Normal,
+        gpio::SlewRate::Standard,
+    );
+
+    let button = gpio::Input::new(p.PIO1_2, gpio::Pull::Down, gpio::Inverter::Disabled);
+
+    // Create a new FlexSPI NOR flash driver.
+    // NOTE: This relies on the FlexSPI having been configured already by having a valid FCB in the flash memory.
+    let mut flash = unsafe { FlexSpiNorFlash::with_probed_config(p.FLEXSPI.reborrow(), 2, 2) }
+        .map_err(|e| defmt::error!("Failed to initialize FlexSPI peripheral: {}", e))
+        .unwrap();
+
+    // Read last sector.
+    let mut last_sector = [0; 16];
+    let last_sector_addr = 0x1F_F000;
+    flash
+        .read(last_sector_addr, &mut last_sector)
+        .map_err(|e| defmt::error!("Failed to read last sector from flash: {}", e))
+        .unwrap();
+    defmt::info!("Last sector[0..16] (pre-erase): {}", last_sector[0..16]);
+
+    unsafe { flash.erase_sector(last_sector_addr) }
+        .map_err(|e| defmt::error!("Failed to erase last sector from flash: {}", e))
+        .unwrap();
+
+    flash
+        .read(last_sector_addr, &mut last_sector)
+        .map_err(|e| defmt::error!("Failed to read last sector from flash: {}", e))
+        .unwrap();
+    defmt::info!("Last sector[0..16] (post-erase): {}", last_sector);
+
+    // Program last sector.
+    defmt::info!("Programming first chunk of last sector");
+    unsafe {
+        flash.page_program(
+            last_sector_addr,
+            &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        )
+    }
+    .map_err(|e| defmt::error!("Failed to program first chunk of last sector: {}", e))
+    .unwrap();
+    defmt::info!("Programmed first chunk of last sector");
+
+    flash
+        .read(last_sector_addr, &mut last_sector)
+        .map_err(|e| defmt::error!("Failed to read last sector from flash: {}", e))
+        .unwrap();
+    defmt::info!("Last sector[0..16] (post-program): {}", last_sector);
+
+    loop {
+        info!("Toggling LED");
+        led.toggle();
+        Timer::after_millis(1000).await;
+
+        if button.is_low() {
+            info!("Button pressed");
+        } else {
+            info!("Button not pressed");
+        }
+    }
+}
